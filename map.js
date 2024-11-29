5c5208e9dbbe801090cc9a4dfd82c97c272c07dd
zoevosslee <zvl@mit.edu>
2024-11-29 09:51:04 -0500

    Updated map.js to include Stadia Tiles and fixed GeoJSON retrieval

--- map.js
+++ map.js
@@ -1,10 +1,29 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoienZsMTIxNSIsImEiOiJjbTE4OG1lNnQwOG5lMmpxMnRwNGZnb3drIn0.U_npUNUZEOSOXVi5-SWgHw';
 
+// Initialize the map
 const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/zvl1215/cm1dtqpo000qf01qk450z2pv4', // Your custom style
+  container: 'map', // The ID of the map container
+  style: {
+    version: 8,
+    sources: {
+      'stadia-tiles': {
+        type: 'raster',
+        tiles: [
+          `https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}.png?api_key=99e82fc4-ae1b-4d11-8f3e-1aebf67508f9`
+        ],
+        tileSize: 256,
+      },
+    },
+    layers: [
+      {
+        id: 'stadia-tiles-layer',
+        type: 'raster',
+        source: 'stadia-tiles',
+      },
+    ],
+  },
   center: [-73.935242, 40.730610], // Starting position [lng, lat]
-  zoom: 12
+  zoom: 12, // Initial zoom level
 });
 
 map.on('load', () => {
@@ -21,9 +40,9 @@ map.on('load', () => {
     source: 'mongoLayer',
     paint: {
       'circle-radius': 10,
-      'circle-color': '#ffd500',
-      'circle-stroke-color': '#000000', // Outline color (black)
-      'circle-stroke-width': .5 // Outline width
+      'circle-color': '#ffd500', // Fill color
+      'circle-stroke-color': '#000000', // Outline color
+      'circle-stroke-width': 0.5 // Outline width
     }
   });
 });
