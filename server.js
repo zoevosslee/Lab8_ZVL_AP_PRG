5c601540ecd43b79e2c546332f1323ca9b246cbd
zoevosslee <zvl@mit.edu>
2024-12-04 15:30:07 -0500

    bug fixes

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname,'')));
+
 // Middleware for cross-origin requests and parsing form data
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
