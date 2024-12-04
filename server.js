d25d62b99fb47f693c3adc774071750f66cdf587
zoevosslee <zvl@mit.edu>
2024-12-04 00:14:41 -0500

    added place pages

--- server.js
+++ server.js
@@ -83,22 +83,13 @@ app.get('/api/geojson', async (req, res) => {
   }
 });
 
-// Serve HTML files
-app.get('/home-page', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'home-page.html'));
-});
-
-app.get('/about', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'about.html'));
-});
-
-app.get('/map-explore', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'map-explore.html'));
-});
-
 // Serve the form HTML
 app.get('/form', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'Form.html'));
+});
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 // Handle form submissions
