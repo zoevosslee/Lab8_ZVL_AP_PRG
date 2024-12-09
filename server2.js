3357374158126e073e040511bb529aafb0474795
zoevosslee <zvl@mit.edu>
2024-12-09 14:46:57 -0500

    edit

--- server2.js
+++ server2.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(express.static(path.join(__dirname,'')));
+app.use(express.static(path.join(__dirname,'')));
 app.use(express.static('public'));
 
 // Middleware for cross-origin requests and parsing form data
